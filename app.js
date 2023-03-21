fe175b54180b8cf1cc9809a6a87a0b379ffceecb
Muhammad Atta ul Mustafa <120771673+Atta1380@users.noreply.github.com>
2023-03-21 21:11:59 +0500

    added a html and javascript files

---
+++ app.js
@@ -0,0 +1,30 @@
+var firstname = prompt("what is your first name ?");
+var lastname = prompt("what is your last name ?");
+
+var fullname = firstname +" " + lastname;
+
+alert("Hello, " + fullname + "!");
+
+
+
+
+
+
+// var favoritephone = prompt("what is your favorite mobile phone modle");
+// var modlelength = favoritephone.length;
+
+// alert("my favorite phone is : " + favoritephone  +  "\nlength of string " + modlelength);
+
+
+// var myword = "pakistani";
+// var wordindex = myword.indexOf("n");
+
+// alert("string " + myword + "\nIndex of 'n': " + wordindex);
+
+
+
+// var endword = "Hello World";
+// var lastindex = endword.lastIndexOf("l");
+
+
+// document.write()
